68ed2371f4d69cc18bbcb4378ae3bc8bb73b8657
NGUYỄN SỸ HẢI THANH <thanhproo1412@gmail.com>
2024-04-19 16:26:41 +0700

    add

--- cypress/integration/pages/HomePage.js
+++ cypress/integration/pages/HomePage.js
@@ -1,10 +1,7 @@
 class HomePage {
 
 
-  //Basic Auth btn
-  getLoginBtn() {
-    return cy.get('div[id="content"] a:contains("Basic Auth")');
-  }
+
 
   // AB Testing btn
   getABTestingBtn() {
@@ -15,6 +12,207 @@ class HomePage {
   getAddRemoveElementsBtn() {
     return cy.get('div[id="content"] a:contains("Add/Remove Elements")');
   }
+
+  //Basic Auth btn
+  getBasicAuthBtn() {
+    return cy.get('div[id="content"] a:contains("Basic Auth")');
+  }
+
+  //Broken Images btn
+  getBrokenImagesBtn() {
+    return cy.get('div[id="content"] a:contains("Broken Images")');
+  }
+
+  //Challenging DOM btn
+  getChallengingDOMBtn() {
+    return cy.get('div[id="content"] a:contains("Challenging DOM")');
+  }
+
+  //Checkboxes btn
+  getCheckboxesBtn() {
+    return cy.get('div[id="content"] a:contains("Checkboxes")');
+  }
+
+  //Context Menu btn
+  getContextMenuBtn() {
+    return cy.get('div[id="content"] a:contains("Context Menu")');
+  }
+
+  //Digest Authentication btn
+  getDigestAuthenticationBtn() {
+    return cy.get('div[id="content"] a:contains("Digest Authentication")');
+  }
+
+  //Disappearing Elements btn
+  getDisappearingElementsBtn() {
+    return cy.get('div[id="content"] a:contains("Disappearing Elements")');
+  }
+
+  //Drag and Drop btn
+  getDragAndDropBtn() {
+    return cy.get('div[id="content"] a:contains("Drag and Drop")');
+  }
+
+  //Dropdown btn
+  getDropdownBtn() {
+    return cy.get('div[id="content"] a:contains("Dropdown")');
+  }
+
+  //Dynamic Content btn
+  getDynamicContentBtn() {
+    return cy.get('div[id="content"] a:contains("Dynamic Content")');
+  }
+
+  //Entry Adbtn
+  getEntryAdBtn() {
+    return cy.get('div[id="content"] a:contains("Entry Ad")');
+  }
+
+  //Exit Intentbtn
+  getExitIntentBtn() {
+    return cy.get('div[id="content"] a:contains("Exit Intent")');
+  }
+
+  //File Download btn
+  getFileDownloadBtn() {
+    return cy.get('div[id="content"] a:contains("File Download")');
+  }
+
+  //File Uploadbtn
+  getFileUploadBtn() {
+    return cy.get('div[id="content"] a:contains("File Upload")');
+  }
+
+  //Floating Menubtn
+  getFloatingMenuBtn() {
+    return cy.get('div[id="content"] a:contains("Floating Menu")');
+  }
+
+  //Forgot Passwordbtn
+  getForgotPasswordBtn() {
+    return cy.get('div[id="content"] a:contains("Forgot Password")');
+  }
+
+  //Form Authenticationbtn
+  getFormAuthenticationBtn() {
+    return cy.get('div[id="content"] a:contains("Form Authentication")');
+  }
+
+  //Framesbtn
+  getFramesBtn() {
+    return cy.get('div[id="content"] a:contains("Frames")');
+  }
+
+  //Geolocationbtn
+  getGeolocationBtn() {
+    return cy.get('div[id="content"] a:contains("Geolocation")');
+  }
+
+  //Horizontal Sliderbtn
+  getHorizontal SliderBtn() {
+    return cy.get('div[id="content"] a:contains("Horizontal Slider")');
+  }
+
+  //Hoversbtn
+  getHoversBtn() {
+    return cy.get('div[id="content"] a:contains("Hovers")');
+  }
+
+  //Infinite Scrollbtn
+  getInfinite ScrollBtn() {
+    return cy.get('div[id="content"] a:contains("Infinite Scroll")');
+  }
+
+  //Inputsbtn
+  getInputsBtn() {
+    return cy.get('div[id="content"] a:contains("Inputs")');
+  }
+
+  //JQuery UI Menus btn
+  getJQueryUIMenusBtn() {
+    return cy.get('div[id="content"] a:contains("JQuery UI Menus")');
+  }
+
+  //JavaScript Alerts btn
+  getJavaScriptAlertsBtn() {
+    return cy.get('div[id="content"] a:contains("JavaScript Alerts")');
+  }
+
+  //JavaScript onload event errorbtn
+  getJavaScriptOnLoadEventerrorBtn() {
+    return cy.get('div[id="content"] a:contains("JavaScript onload event error")');
+  }
+
+  //Key Presses btn
+  getKeyPressesBtn() {
+    return cy.get('div[id="content"] a:contains("Key Presses")');
+  }
+
+  //Large & Deep DOM btn
+  getLargeDeepDOMBtn() {
+    return cy.get('div[id="content"] a:contains("Large & Deep DOM")');
+  }
+
+  //Multiple Windows btn
+  getMultipleWindowsBtn() {
+    return cy.get('div[id="content"] a:contains("Multiple Windows")');
+  }
+
+  //Nested Framesbtn
+  getNestedFramesBtn() {
+    return cy.get('div[id="content"] a:contains("Nested Frames")');
+  }
+
+  //Notification Messages
+  getNotificationMessagesBtn() {
+    return cy.get('div[id="content"] a:contains("Notification Messages")');
+  }
+
+  //Redirect Link
+  getRedirectLinkBtn() {
+    return cy.get('div[id="content"] a:contains("Redirect Link")');
+  }
+
+  //Secure File Download
+  getSecureFileDownloadBtn() {
+    return cy.get('div[id="content"] a:contains("Secure File Download")');
+  }
+
+  //Shadow DOM
+  getShadowDOMBtn() {
+    return cy.get('div[id="content"] a:contains("Shadow DOM")');
+  }
+
+  //Shifting Content
+  getShiftingContentBtn() {
+    return cy.get('div[id="content"] a:contains("Shifting Content")');
+  }
+
+  //Slow Resources btn
+  getSlowResourcesBtn() {
+    return cy.get('div[id="content"] a:contains("Slow Resources")');
+  }
+
+  //Sortable Data Tables btn
+  getSortableDataTablesBtn() {
+    return cy.get('div[id="content"] a:contains("Sortable Data Tables")');
+  }
+
+  //Status Codes btn
+  getStatusCodesBtn() {
+    return cy.get('div[id="content"] a:contains("Status Codes")');
+  }
+
+  //Typos btn
+  getTyposBtn() {
+    return cy.get('div[id="content"] a:contains("Typos")');
+  }
+
+  //WYSIWYG Editor btn
+  getWYSIWYGEditorBtn() {
+    return cy.get('div[id="content"] a:contains("WYSIWYG")');
+  }
+
 
   //WYSIWYG Editor btn
   getWYSIWYGEditorBtn() {
